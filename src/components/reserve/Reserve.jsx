0aa2256f3083742e06be38729ab0e43276338d1b
temple-19 <tozoaz@gmail.com>
2023-04-12 12:49:23 +0100

    url change

--- src/components/reserve/Reserve.jsx
+++ src/components/reserve/Reserve.jsx
@@ -10,7 +10,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Reserve = ({ setOpen, hotelId }) => {
   const [selectedRooms, setSelectedRooms] = useState([]);
-  const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
+  const { data, loading, error } = useFetch(`https://dark-frog-dungarees.cyclic.app/api/hotels/room/${hotelId}`);
   const { dates } = useContext(SearchContext);
 
   const getDatesInRange = (startDate, endDate) => {
