a3ed17f157300b5caae7e32ead1a5ca1d990f362
temple-19 <tozoaz@gmail.com>
2023-04-14 21:36:13 +0100

    https://chipper-pie-6f9780.netlify.app/

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
               <span className="option">Register</span>
             </Link>
             <Link
-              to="/login"
+              to="https://chipper-pie-6f9780.netlify.app/login"
               style={{ color: 'white', textDecoration: 'none' }}
             >
               <span className="option">Login</span>
