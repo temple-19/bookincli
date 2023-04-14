9f57800882669d4966fb0dba7e70f09274ff5c44
temple-19 <tozoaz@gmail.com>
2023-04-14 11:41:39 +0100

    url to https://dark-frog-dungarees.cyclic.app/api

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (url) => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const res = await axios.get(url);
+        const res = await axios.get("https://dark-frog-dungarees.cyclic.app/api");
         setData(res.data);
       } catch (err) {
         setError(err);
@@ -18,12 +18,12 @@ const useFetch = (url) => {
       setLoading(false);
     };
     fetchData();
-  }, [url]);
+  }, ["https://dark-frog-dungarees.cyclic.app/api"]);
 
   const reFetch = async () => {
     setLoading(true);
     try {
-      const res = await axios.get(url);
+      const res = await axios.get("https://dark-frog-dungarees.cyclic.app/api");
       setData(res.data);
     } catch (err) {
       setError(err);
