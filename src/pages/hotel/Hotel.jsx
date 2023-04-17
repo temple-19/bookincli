9f99614f833ce9c906299ead47bd6f82416a199c
temple-19 <tozoaz@gmail.com>
2023-04-17 08:56:04 +0100

    Update Hotel.jsx

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -37,7 +37,7 @@ const Hotel = () => {
     return diffDays;
   }
 
-  const days = dayDifference(dates[0]?.endDate, dates[0]?.startDate);
+  const days = dayDifference(dates[0].endDate, dates[0].startDate);
 
   const handleOpen = (i) => {
     setSlideNumber(i);
