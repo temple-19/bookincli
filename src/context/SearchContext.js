089c5df71d6351615d5555e6f70776859e7e192d
temple-19 <tozoaz@gmail.com>
2023-04-19 16:25:42 +0100

    up1.2

--- src/context/SearchContext.js
+++ src/context/SearchContext.js
@@ -1,6 +1,16 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 
 const INITIAL_STATE = {
+  city: 'undefined' || JSON.parse(localStorage.getItem('destination')),
+  dates: JSON.parse(localStorage.getItem('dates')) || [],
+  options: JSON.parse(localStorage.getItem('options')) || {
+    adult: undefined,
+    children: undefined,
+    room: undefined,
+  },
+};
+
+const RESET_STATE = {
   city: undefined,
   dates: [],
   options: {
@@ -9,15 +19,17 @@ const INITIAL_STATE = {
     room: undefined,
   },
 };
-
 export const SearchContext = createContext(INITIAL_STATE);
 
 const SearchReducer = (state, action) => {
   switch (action.type) {
     case 'NEW_SEARCH':
-      return action.payload;
+      return {
+        dates: action.payload.dates,
+        options: action.payload.options,
+      };
     case 'RESET_SEARCH':
-      return INITIAL_STATE;
+      return RESET_STATE;
     default:
       return state;
   }
@@ -26,10 +38,17 @@ const SearchReducer = (state, action) => {
 export const SearchContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(SearchReducer, INITIAL_STATE);
 
+  useEffect(() => {
+    localStorage.setItem('dates', JSON.stringify(state.dates));
+  }, [state.dates]);
+  useEffect(() => {
+    localStorage.setItem('options', JSON.stringify(state.options));
+  }, [state.options]);
+
   return (
     <SearchContext.Provider
       value={{
-        city: state.city,
+        destination: state.city,
         dates: state.dates,
         options: state.options,
         dispatch,
